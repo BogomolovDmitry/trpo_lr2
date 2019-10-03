a110d699d7054bd37616499ccc8efd334d6fbc14
Denizche <Denizche1204@gmail.com>
2019-10-04 01:12:26 +0300

    Fix3

--- askname.h
+++ askname.h
@@ -1,11 +1,3 @@
 /* askname.h */
 void askname(char *first, char *last);
-<<<<<<< HEAD
 //Важные изменения 2.2
-=======
-<<<<<<< HEAD
-//Важные изменения 2.5
-=======
-//Важные изменения
->>>>>>> parent of 5377865... Some improvements
->>>>>>> 7b2e3759b883730efab9ce7e4cd122223e5f003f
